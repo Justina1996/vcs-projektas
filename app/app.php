42a8ce2f9df54f0d958334251aa444effec201bb
Justina1996 <45970340+Justina1996@users.noreply.github.com>
2020-04-10 08:58:47 +0300

    Add files via upload

--- app/app.php
+++ app/app.php
@@ -8,18 +8,6 @@
         $message = trim($_POST['message']);
     
         if(!empty($name) && !empty($email) && !empty($phone) && !empty($message)) {
-            if(filter_var ($email, FILTER_VALIDATE_EMAIL)) {
-                $from = "$email";
-                $to = "aukstuolytei@gmail.com";
-                $subject = "Nauja zinute";
-                //$autorius = 'Nuo: ' . $name . ', ' . $email;
-                $zinute = htmlspecialchars($message);
-
-                $headers['From'] = $from;
-
-                mail($to, $subject, $zinute, $headers);
-            }
-            
             insert_into_database($name, $email, $phone, $message);
             echo "<script>alert('Dekojame. Jusu uzklausa gauta. Netrukus susisieksime.');</script>";
         }
